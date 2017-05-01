9ae4330d78acd20bea3090c7172f44ca07b1f466
DELL <DELL@192.168.0.14>
2017-05-01 15:37:49 +0530

    dev1-delete

--- n1/src/main/java/com/nt/demo/TestApp.java
+++ n1/src/main/java/com/nt/demo/TestApp.java
@@ -5,7 +5,7 @@ public class TestApp {
 	public static void main(String[] args) {
 	System.out.println("welcome to git");
 	System.out.println("welcome to git1");
-	System.out.println("welcome to git2");
+	
 
 
 
