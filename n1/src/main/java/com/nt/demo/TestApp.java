bff765b6bb24997db1b6944697ed977fcc94966e
DELL <DELL@192.168.0.14>
2017-05-01 15:31:44 +0530

    dev2-change2

--- n1/src/main/java/com/nt/demo/TestApp.java
+++ n1/src/main/java/com/nt/demo/TestApp.java
@@ -5,6 +5,8 @@ public class TestApp {
 	public static void main(String[] args) {
 	System.out.println("welcome to git");
 	System.out.println("welcome to git1");
+	System.out.println("welcome to git2");
+
 
 
 	}
